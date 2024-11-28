5dc547b9fd5985a2a6efde657d2a338be4ebff07
Steff Beckers <steff@steffbeckers.eu>
2024-11-28 21:20:30 +0100

    Startup projects generated

---
+++ Playground.WPFApp/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Playground.WPFApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
