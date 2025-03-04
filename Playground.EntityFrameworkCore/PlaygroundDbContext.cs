fe59b4915f62e3e95f234c8b26f4c2ea8458903b
Steff Beckers <steff@steffbeckers.eu>
2025-03-04 22:29:00 +0100

    Project updates

---
+++ Playground.EntityFrameworkCore/PlaygroundDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Playground.EntityFrameworkCore;
+
+public class PlaygroundDbContext : DbContext
+{
+    public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<WeatherForecast>(b =>
+        {
+            b.Property(x => x.Date).IsRequired();
+            b.Property(x => x.TemperatureC).IsRequired();
+            b.Property(x => x.Summary).HasMaxLength(100);
+        });
+    }
+}
---
+++ Playground.xUnit/WeatherForecastTests.cs
@@ -0,0 +1,26 @@
+﻿using Shouldly;
+
+namespace Playground.xUnit;
+
+public class WeatherForecastTests
+{
+    [Fact]
+    public void Should_Create_New_WeatherForecast()
+    {
+        // Act
+        WeatherForecast weatherForecast = new WeatherForecast();
+
+        // Assert
+        Assert.NotNull(weatherForecast);
+    }
+
+    [Fact]
+    public void Should_Create_New_WeatherForecast_Shouldly()
+    {
+        // Act
+        WeatherForecast weatherForecast = new WeatherForecast();
+
+        // Assert
+        weatherForecast.ShouldNotBe(null);
+    }
+}
