f3f7825ee8a27c5441467d228093f461c5f73e26
Steff Beckers <steff@steffbeckers.eu>
2025-12-21 10:06:45 +0100

    Wimbledon

---
+++ Playground.ClassLibrary/ProgrammingAdvancedCup/Wimbledon.cs
@@ -0,0 +1,31 @@
+﻿namespace Playground.ClassLibrary.ProgrammingAdvancedCup;
+
+public class Wimbledon
+{
+    public string Decrypt(string encryptedMessage)
+    {
+        string decryptedMessage = string.Empty;
+
+        int indexFromStart = 0;
+        int indexFromEnd = encryptedMessage.Length - 1;
+        bool fromStart = true;
+
+        while (decryptedMessage.Length != encryptedMessage.Length)
+        {
+            if (fromStart)
+            {
+                decryptedMessage += encryptedMessage[indexFromStart];
+                indexFromStart++;
+            }
+            else
+            {
+                decryptedMessage += encryptedMessage[indexFromEnd];
+                indexFromEnd--;
+            }
+
+            fromStart = !fromStart;
+        }
+
+        return decryptedMessage;
+    }
+}
---
+++ Playground.xUnit/ProgrammingAdvancedCup/WimbledonTests.cs
@@ -0,0 +1,30 @@
+﻿using Playground.ClassLibrary.ProgrammingAdvancedCup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground.xUnit.ProgrammingAdvancedCup;
+
+public class WimbledonTests
+{
+    private readonly Wimbledon _wimbledon;
+
+    public WimbledonTests()
+    {
+        _wimbledon = new Wimbledon();
+    }
+
+    [Theory]
+    [InlineData(
+        "Oea  ie.mtat n",
+        "One at a time.")]
+    [InlineData(
+        "W'entwies u o,cnw ev.erse a ybtb,rta o re",
+        "We're not waiters, but boy, can we serve.")]
+    public void Should_Decrypt_Message(string encryptedMessage, string decryptedMessage)
+    {
+        Assert.Equal(decryptedMessage, _wimbledon.Decrypt(encryptedMessage));
+    }
+}
